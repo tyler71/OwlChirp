fde3d10eaa90c4fcb4c6b7be126139d2f9573de4
Tyler71 <tyler71@example.com>
2023-02-07 08:05:18 -0800

    one spinner left in (handled incoming)

--- app/static/scripts/core.js
+++ app/static/scripts/core.js
@@ -417,16 +417,16 @@ function _realtimeUpdateAvailableCount(data) {
 }
 
 async function _realtimeUpdateHandledIncoming(data) {
+    const ALT_LOADING_CLASS = 'spinner-border'
     let handledIncomingElement = document.querySelector('#handledIncoming');
-    if (handledIncomingElement.classList.contains(LOADING_CLASS)) {
-        handledIncomingElement.classList.remove(LOADING_CLASS)
+    if (handledIncomingElement.classList.contains(ALT_LOADING_CLASS)) {
+        handledIncomingElement.classList.remove(ALT_LOADING_CLASS)
     }
     let handledCalls = data.handled_incoming
     handledIncomingElement.innerHTML = handledCalls;
 }
 
 async function _realtimeUpdateVisualAgentList(data) {
-    console.log(data)
     let visualAgentList = document.querySelector('#visualAgentList');
     if (visualAgentList.classList.contains(LOADING_CLASS)) {
         visualAgentList.classList.remove(LOADING_CLASS)
