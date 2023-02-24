c10deb1707b1f6f068118059969d6cda5c43d9a7
tyler71 <sagsdf_github_tyler71@xyzz.work>
2023-02-24 09:56:31 -0800

    Setup linking for compiling

--- app/main.sh
+++ app/main.sh
@@ -6,6 +6,9 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
   cd client
   npm install
   npm run prod
+  mkdir -p "$SCRIPT_DIR"/server/static/dist
+  rm "$SCRIPT_DIR"/server/static/dist/main.*.js
+  ln -s dist/main.*.js "$SCRIPT_DIR"/server/static/dist
 )
 
 python "$SCRIPT_DIR"/server
