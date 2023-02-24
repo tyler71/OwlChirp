507215f08d0c9a0ccdced85355ae09d16db870aa
tyler71 <sagsdf_github_tyler71@xyzz.work>
2023-02-24 09:31:49 -0800

    Setup main.sh

--- app/main.sh
+++ app/main.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+set -xv
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+(
+  cd "$SCRIPT_DIR"
+  cd client
+  npm install
+  npm run prod
+)
 
-python /app
+python "$SCRIPT_DIR"/server
